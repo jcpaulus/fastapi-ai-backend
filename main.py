628931ddda67bc0caa43e438f5e851392fa4792f
Johanes Christian Paulus <johaneschristianpaulus@Johaness-MacBook-Air.local>
2026-03-12 00:13:42 -0700

    fix cors position

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from fastapi.middleware.cors import CORSMiddleware
 from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
 import sqlite3
@@ -6,6 +7,14 @@ import os
 import requests
 
 app = FastAPI()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 
 GROQ_API_KEY = os.getenv("GROQ_API_KEY")
 GROQ_URL = "https://api.groq.com/openai/v1/chat/completions"
